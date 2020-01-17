c0f2658ea5bd937eabcad90170e40bef3dc698ff
Bruno Unna <bunna@getvisibility.com>
2020-01-17 16:53:38 +0000

    Add suggestions to the solution

--- Exercise2/exercise2/src/test/scala/example/RecursiveListSearchSpec.scala
+++ Exercise2/exercise2/src/test/scala/example/RecursiveListSearchSpec.scala
@@ -1,15 +1,19 @@
 package example
 
-import org.scalatest._
+import org.scalatest.{FlatSpec, Matchers}
 
 class RecursiveListSearchSpec extends FlatSpec with Matchers {
+  "The RecursiveListSearch" should "return the nth element of a list of integers" in {
+    assert(RecursiveListSearch.nthRecursive(2, List(3, 2, 1, 0, -1, -2)) == 1)
+  }
+
   "The RecursiveListSearch" should "return the nth element of a list" in {
-    assert(RecursiveListSearch.nthRecursive(4, List("a","b","c","d","e")) == "e") ;
+    assert(RecursiveListSearch.nthRecursive(4, List("a", "b", "c", "d", "e")) == "e")
   }
 
   "The RecursiveListSearch" should "return an error for " in {
     intercept[NoSuchElementException] {
-      RecursiveListSearch.nthRecursive(6, List("a", "b", "c", "d", "e"));
+      RecursiveListSearch.nthRecursive(6, List("a", "b", "c", "d", "e"))
     }
   }
 }
